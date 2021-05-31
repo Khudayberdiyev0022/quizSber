481147ca5b4f7acadcd3ecff11548e7b9df407f0
Khudayberdiyev0022 <khamza.khudayberdiyev@gmail.com>
2021-05-31 17:55:49 +0500

    12

--- src/components/Usefull/Usefull.js
+++ src/components/Usefull/Usefull.js
@@ -1,27 +1,32 @@
 import React from "react";
 import style from "./Usefull.module.css";
 import img from "../../assets/svg/download.svg";
+import apple from "../../assets/svg/apple.svg";
+import android from "../../assets/svg/android.svg";
+import windows from "../../assets/svg/windows.svg";
 import sber from "../../assets/sber.pdf";
 
 const Usefull = () => {
   const data = [
     {
       id: 0,
-      title: "Чек-лист по цифровой гигиене ",
-      mb: "3,5",
+      title: "Чек-лист по цифровой гигиене",
+      mb: "3,8",
       pdf: sber,
+      iOS: null,
+      android: null,
+      macOS: null,
+      Windows: null,
     },
     {
       id: 1,
       title: "Памятки настройки безопасности гаджетов для защиты ",
-      mb: "3,5",
-      pdf: sber,
-    },
-    {
-      id: 2,
-      title: "Чек-лист по цифровой гигиене ",
-      mb: "3,5",
-      pdf: sber,
+      mb: null,
+      pdf: null,
+      iOS: apple,
+      android: android,
+      macOS: apple,
+      Windows: windows,
     },
   ];
   return (
@@ -29,22 +34,71 @@ const Usefull = () => {
       <div className={style.container}>
         <h2 id="usefull">Полезные материалы</h2>
         <div className={style.cards}>
-          {data.map((item) => (
+          {data.map((item, index) => (
             <div key={item.id} className={style.card}>
               <div>
-                <h4>Презентация PDF</h4>
                 <h3>{item.title}</h3>
               </div>
               <div className={style.bottom}>
-                <h4>{item.mb} Mb</h4>
-                <a
-                  href={item.pdf}
-                  className={style.image}
-                  rel="noreferrer"
-                  download
-                >
-                  <img src={img} alt="svg" />
-                </a>
+                {item.mb && <h4>{item.mb} Mb (PDF)</h4>}
+                {index === 0 && (
+                  <a
+                    href={item.pdf}
+                    className={style.image}
+                    rel="noreferrer"
+                    download
+                  >
+                    <img src={img} alt="svg" />
+                  </a>
+                )}
+                {item.iOS && (
+                  <div className={style.operationBlock}>
+                    <div className={style.operationFlex}>
+                      <a
+                        href="https://www.sberbank.ru//common/img/uploaded/redirected/promo.sber.ru/kidssafety/docs/pamyatka_nastroiki_bezopasnosti_ios_dlya_zashchity_dete%C4%AD_compressed.pdf"
+                        target="__blank"
+                        style={{ textDecoration: "none" }}
+                      >
+                        <div className={style.operationBox}>
+                          <img src={item.iOS} alt="iOS" />
+                          <p>iOS</p>
+                        </div>
+                      </a>
+                      <a
+                        href="https://www.sberbank.ru//common/img/uploaded/redirected/promo.sber.ru/kidssafety/docs/pamyatka_nastroiki_bezopasnosti_android_24_05(1)_compressed.pdf"
+                        target="__blank"
+                        style={{ textDecoration: "none" }}
+                      >
+                        <div className={style.operationBox}>
+                          <img src={item.android} alt="android" />
+                          <p>Android</p>
+                        </div>
+                      </a>
+                    </div>
+                    <div className={style.operationFlex}>
+                      <a
+                        href="https://www.sberbank.ru//common/img/uploaded/redirected/promo.sber.ru/kidssafety/docs/pamyatka_nastroiki_bezopasnosti_komp'yutery_macos_24_05(1)_compressed.pdf"
+                        target="__blank"
+                        style={{ textDecoration: "none" }}
+                      >
+                        <div className={style.operationBox}>
+                          <img src={item.macOS} alt="macOS" />
+                          <p>Mac OS</p>
+                        </div>
+                      </a>
+                      <a
+                        href="https://www.sberbank.ru//common/img/uploaded/redirected/promo.sber.ru/kidssafety/docs/pamyatka_nastroiki_bezopasnosti_komp'yutery_windows_24_05_compressed.pdf"
+                        target="__blank"
+                        style={{ textDecoration: "none" }}
+                      >
+                        <div className={style.operationBox}>
+                          <img src={item.Windows} alt="Windows" />
+                          <p>Windows</p>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           ))}
